6ee79ff8ddc5eb162ae5640c3d8c842dfcf2e9db
vlbu0001 <113466734+bukh04@users.noreply.github.com>
2024-10-22 08:43:08 +0200

    Add project files.

---
+++ bingo/Plader.cs
@@ -0,0 +1,25 @@
+﻿namespace bingo;
+
+public class Plader
+{
+    public List<string[]> ID1 = new List<string[]>
+    {
+        new string[] { "5", "31", "40", "51", "85" },
+        new string[] { "7", "14", "22", "34", "87" },
+        new string[] { "8", "26", "45", "69", "73" }
+    };  
+
+    public List<string[]> ID2 = new List<string[]>
+    {
+        new string[] { "20", "33", "43", "51", "80" },
+        new string[] { "4", "21", "36", "47", "73" },
+        new string[] { "8", "17", "24", "57", "69" }
+    };
+
+    public List<string[]> ID3 = new List<string[]>
+    {
+        new string[] { "15", "23", "51", "71", "85" },
+        new string[] { "17", "28", "43", "62", "72" },
+        new string[] { "9", "39", "45", "57", "67" }
+    };
+}
---
+++ bingo/Program.cs
@@ -0,0 +1,104 @@
+﻿using bingo;
+
+class Bingo
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Select the number of plader (max 3)");
+        string plader = Console.ReadLine();
+
+        //get copy object Plader
+        var allPlades = new Plader();
+
+        List<List<string[]>> gamePlade = new List<List<string[]>>();
+
+        switch (plader)
+        {
+            case "1":
+                gamePlade.Add(allPlades.ID1);
+                break;
+
+            case "2":
+                gamePlade.Add(allPlades.ID1);
+                gamePlade.Add(allPlades.ID2);
+                break;
+
+            case "3":
+                gamePlade.Add(allPlades.ID1);
+                gamePlade.Add(allPlades.ID2);
+                gamePlade.Add(allPlades.ID3);
+                break;
+
+            default:
+                Console.WriteLine("Invalid input");
+                return;
+        }
+
+        Console.WriteLine("Selected plader:");
+        foreach (var list in gamePlade)
+        {
+            foreach (var row in list)
+            {
+                Console.WriteLine(string.Join(", ", row)); 
+            }
+            Console.WriteLine(); 
+        }
+        StartGame(gamePlade);
+    }
+
+    static void StartGame(List<List<string[]>> plader)
+    {
+        var calledNumbers = new List<string>();
+        bool hasWinner = false;
+        int drawnNumber;
+        while (true)
+        {
+            Console.WriteLine("Enter a number between 1 and 90:");
+            if (!int.TryParse(Console.ReadLine(), out drawnNumber) || drawnNumber <= 1 || drawnNumber >= 90)
+            {
+                Console.WriteLine("Invalid number");
+                continue;
+            }
+
+            string num = drawnNumber.ToString();
+
+            if (!calledNumbers.Contains(num))
+            {
+                calledNumbers.Add(num);
+            }
+            Console.Clear();
+            Console.WriteLine($" Drawn number: {num} \n Updated plader:");
+
+            foreach (var plade in plader)
+            {
+                foreach (var row in plade)
+                {
+                    for (int i = 0; i < row.Length; i++)
+                    {
+                        if (calledNumbers.Contains(row[i]))
+                        {
+                            Console.Write("X\t"); 
+                            row[i] = "X"; 
+                        }
+                        else
+                        {
+                            Console.Write(row[i] + "\t"); 
+                        }
+                    }
+                    Console.WriteLine();
+                    if (row.All(x => x.Contains("X")))
+                    {
+                        hasWinner = true;
+                    }
+                }
+                Console.WriteLine();
+            }
+            if (hasWinner)
+            {
+                Console.WriteLine("We have a winner,  tillyke!");
+                break;
+            }
+            Console.WriteLine();
+        }
+    }
+}
